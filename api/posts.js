9da323365584e98db81117737d6f46a13d16a188
Warren Waterworth <warren.waterworth@gmail.com>
2022-03-19 13:49:57 -0600

    GET on posts will only return inactive posts if they belong to logged in user

--- api/posts.js
+++ api/posts.js
@@ -98,12 +98,20 @@ postsRouter.use((req, res, next) => {
   next();
 });
 
-postsRouter.get("/", async (req, res) => {
-  const posts = await getAllPosts();
+postsRouter.get("/", async (req, res, next) => {
+  try {
+    const allPosts = await getAllPosts();
 
-  res.send({
-    posts,
-  });
+    const posts = allPosts.filter((post) => {
+      return post.active || (req.user && post.author.id === req.user.id);
+    });
+
+    res.send({
+      posts,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = postsRouter;
