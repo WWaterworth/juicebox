463188364497358ee32fe91116cdbccd5deef8a5
Warren Waterworth <warren.waterworth@gmail.com>
2022-03-19 14:06:07 -0600

    GET posts by tagname filters posts which are both inactive and not owned by current user

--- api/tags.js
+++ api/tags.js
@@ -9,18 +9,17 @@ tagsRouter.use((req, res, next) => {
 });
 
 tagsRouter.get("/:tagName/posts", async (req, res, next) => {
-  // read the tagname from the params
   const { tagName } = req.params;
   try {
-    // use our method to get posts by tag name from the db
-    const posts = await getPostsByTagName(tagName);
-    console.log("posts", posts);
-    // send out an object to the client { posts: // the posts }
+    const allPosts = await getPostsByTagName(tagName);
+    const posts = allPosts.filter((post) => {
+      return post.active || (req.user && post.author.id === req.user.id);
+    });
+
     res.send({
       posts: posts,
     });
   } catch ({ name, message }) {
-    // forward the name and message to the error handler
     next({ name, message });
   }
 });
